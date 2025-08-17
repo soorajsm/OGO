36e5dcbe872cc3cddf3e7b985c4b7bc85fd0dd8f
sooraj <soorajsm56@gmail.com>
2025-08-17 23:21:24 +0530

    add changes

--- arraynslices.go
+++ arraynslices.go
@@ -5,6 +5,12 @@ import "fmt"
 func arraynslice(){
 	var arr = [3]string{"one","two","three"}
 	var slice = []string{"one","two","three","four"}
-	fmt.Println(arr)
-	fmt.Println(slice)
+
+	for index,value:=range arr {
+		fmt.Println(index,value)
+	}
+
+	for index,value:=range slice {
+		fmt.Println(index,value)
+	}
 }
